da34a23292a2fd8042314832f168328ab6a37f05
Marshal-bd <arfurm198@gmail.com>
2025-11-07 21:37:47 +0700

    Commit 10

--- routes/struk.routes.js
+++ routes/struk.routes.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { addStruk } from "../controller/struk.controller.js";
+import {
+  addStruk,
+  updateStruk,
+  findStruk,
+} from "../controller/struk.controller.js";
 import { Authorize } from "../controller/users.controller.js";
 import { isAdmin } from "../middleware/roleValidation.js";
 
 const app = express();
 
 app.post("/AddStruk", Authorize, isAdmin, addStruk);
+app.post("/Update", Authorize, isAdmin, updateStruk);
+app.post("/FindStruk", Authorize, findStruk);
 
 export default app;
